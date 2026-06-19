49ad4e20cc41cc9ce23d859c681f1b9fadee797b
branco0verflow <brann455@gmail.com>
2026-06-19 17:30:42 -0300

    Se cambió la url de AWS S3 a la que ahora usa emanuel

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "vehiculos-okm-imagenes-192292427991-us-east-1-an.s3.us-east-1.amazonaws.com",
+        hostname: "vehiculos-videsol-imagenes.s3.us-east-1.amazonaws.com",
       },
     ],
   },
